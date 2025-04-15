653c6d9d8e0721400701f1b9aa975aa5ba97dda8
MARIA CRISTINA PADRINO <mariacristinapadrino@MacBook-Pro-de-MARIA.local>
2025-04-15 19:41:15 +0200

    fix: simplificación de configuración para nuevo despliegue

--- next.config.js
+++ next.config.js
@@ -3,14 +3,11 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   images: {
+    domains: ['images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'serena-lanka.vercel.app',
       }
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
