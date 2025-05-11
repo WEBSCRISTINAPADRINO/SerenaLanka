94ea60027b446b184c3582d9d9059343ee771737
WEBSCRISTINAPADRINO <afilidadoscristina@gmail.com>
2025-05-11 21:52:03 +0200

    Create MapaCostaSur.tsx

---
+++ src/components/destinos/MapaCostaSur.tsx
@@ -0,0 +1,4 @@
+   import React from "react";
+   export default function MapaCostaSur() {
+     return <div style={{background: 'yellow', padding: 40, textAlign: 'center'}}>Hola desde el mapa</div>;
+   }
